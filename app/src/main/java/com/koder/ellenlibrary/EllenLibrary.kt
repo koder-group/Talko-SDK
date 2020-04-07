a9dde22c67a67f6f2b8603bb9a7b6a897af81385
Jeff H <jeff@koder.com>
2020-04-06 21:58:44 -0700

    Add refresh token functionality and interface

--- app/src/main/java/com/koder/ellenlibrary/EllenLibrary.kt
+++ app/src/main/java/com/koder/ellenlibrary/EllenLibrary.kt
@@ -5,10 +5,17 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
+import android.os.Looper
+import android.util.Log
 import com.koder.ellen.Messenger
+import com.koder.ellen.RequestHandler
 
 
 class EllenLibrary: Application() {
+
+    companion object {
+        const val TAG = "EllenLibrary"
+    }
 
     // Application/Tenant Id
     val appId = "efb94c7e-71e9-406b-9059-61ac05227f1b"
@@ -16,5 +23,18 @@ class EllenLibrary: Application() {
     override fun onCreate() {
         super.onCreate()
 //        Messenger.init(appId, this)
+
+        // Refreshing user token
+        Messenger.addRequestHandler(object: RequestHandler() {
+            override fun onRefreshTokenRequest(): String {
+                Log.d(TAG, "Refreshing token")
+
+                // TODO Implement functionality to return a refreshed user token
+                Thread.sleep(4000)
+                val refreshedToken = "eyJhbGciOiJIUzI1NiIsImtpZCI6IjE3YzRhYmQ4YTE3MjQ0OTdiZmViMjBiMWM0ZDhmYjU0IiwidHlwIjoiSldUIn0.eyJ0ZW5hbnRfaWQiOiJFRkI5NEM3RS03MUU5LTQwNkItOTA1OS02MUFDMDUyMjdGMUIiLCJ1c2VyX2lkIjoiNEVFRDg2Q0UtNDZCNi00NjNGLUJBMjgtQzgzN0IzNDVBRUIzIiwidXNlcl9uYW1lIjoiamVmZmF0a29kZXIiLCJwcm9maWxlX2ltYWdlIjoiaHR0cHM6Ly9maXJlYmFzZXN0b3JhZ2UuZ29vZ2xlYXBpcy5jb20vdjAvYi9lbGxlbi1maXJlYmFzZS1leGFtcGxlLmFwcHNwb3QuY29tL28vQXZhdGFycyUyRnVzZXItMjEucG5nP2FsdD1tZWRpYSZ0b2tlbj1lZjhhYmI1MC0wNjJkLTQ1ZDItOTcwYS1mNDIxNjRmYzA0OWYiLCJleHAiOjE1ODYyNDEzMjAsImlzcyI6Imh0dHBzOi8vZWxsZW4ua29kZXIuY29tL2FwaS9tYW5hZ2VtZW50IiwiYXVkIjoiaHR0cHM6Ly9lbGxlbi5rb2Rlci5jb20vYXBpL21lc3NhZ2luZyJ9.rBh4algbFy64zOojIKRhqKy87dxgP3p5h9-QR9hVrok"
+
+                return refreshedToken
+            }
+        })
     }
 }
--- ellen-android-sdk/src/main/java/com/koder/ellen/api/RetrofitClient.kt
+++ ellen-android-sdk/src/main/java/com/koder/ellen/api/RetrofitClient.kt
@@ -1,12 +1,22 @@
 package com.koder.ellen.api
 
+import android.os.Looper
+import android.util.Base64
+import android.util.Log
+import com.koder.ellen.CompletionCallback
+import com.koder.ellen.Messenger
 import com.koder.ellen.Messenger.Companion.prefs
 import com.koder.ellen.core.AppConstants
+import com.koder.ellen.data.Result
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
+import org.json.JSONObject
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
@@ -30,10 +40,50 @@ object RetrofitClient {
 }
 
 class AuthInterceptor : Interceptor {
+    companion object {
+        const val TAG = "AuthInterceptor"
+    }
+
     override fun intercept(chain: Interceptor.Chain) : Response {
         val request = chain.request().newBuilder()
             .header("Authorization", "Bearer ${prefs?.userToken}")
             .build()
+
+        // Refresh token if current is expired
+        val compareTimestamps = isUserTokenExpired(prefs?.userToken!!)
+//        Log.d(TAG, "compareTimestamps ${compareTimestamps}")
+        if(compareTimestamps > 0) {
+            // current userToken is expired
+
+            // Send refresh token request to host app, synchronously
+            val token = Messenger.refreshToken()
+
+            // Create request with new token
+            val request = chain.request().newBuilder()
+                .header("Authorization", "Bearer ${token}")
+                .build()
+
+            return chain.proceed(request)
+        }
+
         return chain.proceed(request)
+    }
+
+    fun isUserTokenExpired(userToken: String): Int {
+        val currentTimestamp = Calendar.getInstance()
+//        Log.d(TAG, "currentTimestamp ${currentTimestamp}")
+
+        // Decode user token for user info
+        val parts = userToken.split('.')
+        val decoded = Base64.decode(parts[1], Base64.DEFAULT)
+        val decodedStr = String(decoded)
+        val decodedObj = JSONObject(decodedStr)
+
+        val tokenExpiration = decodedObj.getLong("exp")  // seconds
+        val tokenTimestamp = Calendar.getInstance()
+        tokenTimestamp.timeInMillis = tokenExpiration * 1000    // to milliseconds
+//        Log.d(TAG, "tokenExpiration ${tokenTimestamp}")
+
+        return currentTimestamp.compareTo(tokenTimestamp)
     }
 }
