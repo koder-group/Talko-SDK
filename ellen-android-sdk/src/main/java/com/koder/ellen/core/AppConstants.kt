9cd86188cb6e9f734144103db95d41ef221a8afb
Jeff H <jeff@koder.com>
2020-04-13 10:13:54 -0700

    Point SDK to Staging

--- ellen-android-sdk/src/main/java/com/koder/ellen/core/AppConstants.kt
+++ ellen-android-sdk/src/main/java/com/koder/ellen/core/AppConstants.kt
@@ -1,7 +1,7 @@
 package com.koder.ellen.core
 
 internal object AppConstants {
-    const val ELLEN_BASE_URL = "https://kdrellenplatformapimd18.azure-api.net/"
+    const val ELLEN_BASE_URL = "https://kdrellenplatformapims18.azure-api.net/"
     const val FRAGMENT_CODE = 1
     const val CHANNEL_ID = "default"
 }
